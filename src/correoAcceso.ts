a3220117a01347c9308131b6fc188825ebc40a99
RubenE107 <irtu098@gmail.com>
2024-04-06 23:58:30 -0600

    Mejora del mesaje dentro del correo y sweger traducido

--- src/correoAcceso.ts
+++ src/correoAcceso.ts
@@ -16,13 +16,28 @@ var server = email.server.connect({
 
 var message = {
   from: "equipWed@hotmail.com",
-  to: "<"+formulario.correo+">", 
-  bbc : "",
-  subject: "Cambio de contraseña",
+  to: "<" + formulario.correo + ">",
+  bbc: "",
+  subject: "Restablece tu contraseña de Carniceria Don Toño",
   attachment: [
-    { data: `<a href="http://localhost:4200/home/reestablecer/${token}" style="font-family:corbel, arial, sans-serif; font-size:10pt; font-weight: bold;"> Cambiar la contraseña </a>`, alternative: true },
+    {
+      data: `
+      <p>Sentimos saber que has olvidado tu contraseña. ¡Estamos aquí para ayudarte!<br>
+      Parece que has olvidado tu contraseña de Carniceria Don Toño. No te preocupes, ¡te ayudamos a recuperarla!<br>
+      Para restablecer tu contraseña, <a href="${process.env.URlRestablecer}${token}" style="font-family:corbel, arial, sans-serif; font-size:10pt; font-weight: bold;">Haz clip aqui</a></p>
+      <br>
+      <a style="color: red; font-family:corbel, arial, sans-serif; font-size:10pt; font-weight: bold;">Si no has solicitado este restablecimiento:<br>
+      *Ignora este correo electrónico.<br>
+      *Cambia tu contraseña por precaución.<a><br>
+      <br>
+      <p>Atentamente,<br>
+      El equipo de Carniceria Don Toño</p>
+      `,
+      alternative: true,
+    },
   ],
 };
+
 
 server.send(message, function (err:any, message:any) {
   console.log("error=",err);
