3953bc3b107ad47d86a2c1e7bb45410fdc3262e9
RubenE107 <irtu098@gmail.com>
2024-04-08 15:42:35 -0600

    ...

--- src/build/keys.js
+++ src/build/keys.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
+        host: '198.12.216.200',
+        user: 'carnicero',
+        password: 'utm2024compu',
         database: 'carniceria'
     }
 };
